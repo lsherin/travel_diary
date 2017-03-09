e1d4ab109c4d244629088d8d71947b9e855da10d
Lisa Sherin <lisasherin16@gmail.com>
2017-03-09 23:59:48 +0000

    image uploads

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  Paperclip.options[:command_path] = 'C:\Users\lisas\Downloads\ImageMagick-7.0.5-1-portable-Q16-x64'
 
   # Do not eager load code on boot.
   config.eager_load = false
